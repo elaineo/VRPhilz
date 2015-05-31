1a19b795f075b0aea63e76da62f0e020c2df7831
elaineo <elaine.ou@gmail.com>
2015-05-30 23:17:38 -0700

    dump

---
+++ Assets/Cardboard/DemoScene/PictureIcon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PictureIcon : MonoBehaviour {
+	private Vector3 startingSize;
+
+	// Use this for initialization
+	void Start () {
+		startingSize = transform.localScale;
+		SetFocus(false);
+	
+	}
+
+	public void SetFocus(bool gazedAt) {
+		if (gazedAt) {
+			transform.localScale += new Vector3 (0.1F, 0.1F, 0.1F);
+			Debug.Log (transform.localScale);
+		} else {
+			transform.localScale = startingSize;
+		}
+		Debug.Log (GetComponent<Renderer> ().transform.localScale);
+	}
+	
+}
